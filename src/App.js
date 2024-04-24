5baf8ef742d97bb592ec72b8a4236170617ca71e
Sumit <sumit171204@gmail.com>
2024-04-24 22:31:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends React.Component {
           </a>
         </div>
         <div className="footer">
-          <span>Made by <a className="underlined" href="https://github.com/sumit171204" target="_blank" rel="noopener noreferrer">Sumit</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/sumit171204/" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
+          <span>Made by <a className="underlined" href="https://github.com/sumit171204" target="_blank" rel="noopener noreferrer">Sumit</a> using <a className="underlined" href="https://reactjs.org/" title="ReactJS" target="_blank" rel="noopener noreferrer">React</a> | <a className="underlined" href="https://github.com/sumit171204/comingsoon" title="GitHub repo" target="_blank" rel="noopener noreferrer">GitHub</a></span>
         </div>
       </div>
     );
